2f1075266bbf7b81fc61c4fb54418b4b9aa2d714
dioneuty <1@293f3900>
2024-09-08 21:14:11 +0900

    스토어 방식 변경

--- stores/categoryStore.js
+++ stores/categoryStore.js
@@ -1,25 +1,31 @@
 // stores/categoryStore.js
+import { ref, reactive } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCategoryStore = defineStore('category', {
-  state: () => ({
-    categories: [],
-    currentCategory: null
-  }),
-  actions: {
-    async fetchCategories() {
-      //$fetch 사용
-      const categories = await $fetch('/api/categories')
-      this.categories = categories
-    },
-    updateCategoryCount(categoryId, increment = true) {
-      const category = this.categories.find(c => c.id === categoryId)
-      if (category) {
-        category.post_count += increment ? 1 : -1
-      }
-    },
-    setCurrentCategory(category) {
-      this.currentCategory = category
+export const useCategoryStore = defineStore('category', () => {
+  const categories = ref([])
+  const currentCategory = ref(null)
+
+  async function fetchCategories() {
+    categories.value = await $fetch('/api/categories')
+  }
+
+  function updateCategoryCount(categoryId, increment = true) {
+    const category = categories.value.find(c => c.id === categoryId)
+    if (category) {
+      category.post_count += increment ? 1 : -1
     }
+  }
+
+  function setCurrentCategory(category) {
+    currentCategory.value = category
+  }
+
+  return {
+    categories,
+    currentCategory,
+    fetchCategories,
+    updateCategoryCount,
+    setCurrentCategory
   }
 })
